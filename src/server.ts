d7a30853741b0962a1b01122d708297bf2df3e0f
Mayron <mvanleenden@gmail.com>
2019-02-18 09:45:51 +0100

    Api basic setup

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import "reflect-metadata"
+require('dotenv').config()
+// import { SampleController } from './controllers'
+import { createExpressServer, Action } from 'routing-controllers'
+import dbSetup from './db'
+
+
+const app = createExpressServer({
+    controllers: [
+  
+    ],
+    authorizationChecker: async (action: Action) => {
+        return false
+    },
+    currentUserChecker: async (action: Action) => {
+        return undefined
+    },
+    cors:  true
+    
+})
+app.set("port", process.env.PORT || 3000)
+
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
+
+
+io.on('connection', function(socket: any){
+    console.log('a user connected');
+});
+
+
+dbSetup().then(() => {
+    app.listen(app.get("port"), () => {
+        console.log(
+            "App is running on http://localhost:%d in %s mode",
+            app.get("port"),
+            app.get("env")
+        )
+    });
+}).catch((err) => console.error(err))
